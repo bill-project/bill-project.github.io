ce8d75d5eaa3d8fdc935e7aead8a3da4cdd664e9
Christian <chrisdoradotf2@gmail.com>
2019-12-08 01:09:44 -0800

    edited calculator, home page colors, and script

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function get_recommendation() {
   // var username = $("#username_input").val();
   var gpa = $("#gpa_input").val();
+  var sat = $("#sat_input").val();
+  var act = $("#act_input").val();
+  var loanAmount = $("#loanAmount").val();
   console.log("clicked");
   $.ajax({
-    url: "https://Calculator-Backend.sunyu912.repl.co/getschool/" + gpa + "/2/3/4/5",
+    url: "https://Calculator-Backend.sunyu912.repl.co/getschool/" + gpa + "/" + sat + "/" + act + "/4/" + loanAmount,
     success: function(result) {
       $("#result_text").text(result);
     }
